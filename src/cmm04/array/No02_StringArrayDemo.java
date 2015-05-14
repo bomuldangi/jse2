cfa04d874745179aa11294c3e7de14c143f763db
ITBank-Guro <ITBank-Guro@ITBank-Guro-PC>
2015-05-14 11:55:02 +0900

    array

---
+++ src/cmm04/array/No02_StringArrayDemo.java
@@ -0,0 +1,15 @@
+package cmm04.array;
+
+//import java.util.Arrays;
+
+public class No02_StringArrayDemo {
+	public static void main(String[] args) {
+		
+		String[] addr = {"서울","종로","광화문"};
+		for(int i=0; i<addr.length; i++){
+			System.out.print(addr[i]+ " ");
+		}
+		//	System.out.println(Arrays.toString(addr));
+		
+	}
+}
