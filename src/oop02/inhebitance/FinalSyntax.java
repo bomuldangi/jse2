330402f6df2cdfb69532ed88aa1087b5d8f7f6c2
ITBank-Guro <ITBank-Guro@ITBank-Guro-PC>
2015-05-22 11:48:19 +0900

    5/22

---
+++ src/oop02/inhebitance/FinalSyntax.java
@@ -0,0 +1,18 @@
+package oop02.inhebitance;
+/*
+ Date : 15-05-22
+ Author : J
+ Desc : 상속금지 키워드인 final 에 대한 문법
+ 
+ */
+/*
+ @ final
+ 1. final 클래스(abstract class)
+ - final 클래스는 상속 될 수 없다.
+ 2. final 메소드
+ - 자식 클래스가 overriding 할 수 없다.
+ 
+ */
+public class FinalSyntax {
+
+}
---
+++ src/oop02/inhebitance/FruitMain.java
@@ -0,0 +1,19 @@
+package oop02.inhebitance;
+
+public class FruitMain {
+	public static void main(String[] args) {
+		// 인터페이스는 객체를 만들 수 없다.
+		FruitInterface apple = new AppleImpl();
+	
+		FruitInterface orange; // FriutInterface 타입의 참조변수를 선언
+		orange = new OrangeImpl(); // 클래스 OrangeImpl의 객체를 생성하여 Orange에 할당 하였다. 
+		// 위의 apple 선언과 같은 결과다.
+		FruitInterface banana = new BananaImpl();
+		
+		apple.display("맛있는");
+		orange.display("오래된");
+		banana.display("비싼");
+		//orange.getCount();
+		// 인터페이스에 선언된 메소드만 호출이 가능하다.
+	}
+}
